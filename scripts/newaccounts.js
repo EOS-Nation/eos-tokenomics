9c8f8c73ab5a1f6c416699e711073de557946d86
Denis Carriere <carriere.denis@gmail.com>
2024-05-15 19:10:43 +0200

    update actions

---
+++ scripts/newaccounts.js
@@ -0,0 +1,13 @@
+import * as fs from "fs";
+import fees from '../actions/newaccount-eosio.fees.json';
+import reward from '../actions/newaccount-eosio.reward.json';
+import mware from '../actions/newaccount-eosio.mware.json';
+import wram from '../actions/newaccount-market.wram.json';
+
+const actions = [];
+for ( const newaccount of [ fees, reward, mware, wram ] ) {
+    actions.push(...newaccount.actions);
+}
+
+fees.actions = actions;
+fs.writeFileSync('actions/msig-1-newaccounts.json', JSON.stringify(fees, null, 4));
