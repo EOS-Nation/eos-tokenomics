4e3013b1defdb9dfcd1168bd9274b38a437262b7
Denis Carriere <carriere.denis@gmail.com>
2024-05-16 18:29:45 +0200

    add testnet actions

--- scripts/newaccounts.js
+++ scripts/newaccounts.js
@@ -4,10 +4,26 @@ import reward from '../actions/newaccount-eosio.reward.json';
 import mware from '../actions/newaccount-eosio.mware.json';
 import wram from '../actions/newaccount-fund.wram.json';
 
+function createTransaction( actions ) {
+    return {
+        chain_id: fees.chain_id,
+        expiration: fees.expiration,
+        ref_block_num: fees.ref_block_num,
+        ref_block_prefix: fees.ref_block_prefix,
+        actions,
+    }
+};
+
+// Mainnet
 const actions = [];
 for ( const newaccount of [ fees, reward, mware, wram ] ) {
     actions.push(...newaccount.actions);
 }
+fs.writeFileSync('actions/msig-1-newaccounts.json', JSON.stringify(createTransaction(actions), null, 4));
 
-fees.actions = actions;
-fs.writeFileSync('actions/msig-1-newaccounts.json', JSON.stringify(fees, null, 4));
+// Testnets
+const actions_testnet = [];
+for ( const newaccount of [ fees, reward ] ) {
+    actions_testnet.push(...newaccount.actions);
+}
+fs.writeFileSync('actions/testnet-msig-1-newaccounts.json', JSON.stringify(createTransaction(actions_testnet), null, 4));
