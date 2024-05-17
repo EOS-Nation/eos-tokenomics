9ec0f776893423cb9cd6878076c6c224d4cd1a5a
Denis Carriere <carriere.denis@gmail.com>
2024-05-17 23:49:02 +0200

    Update testnet deployement

--- scripts/msig-2-tokenomics.ts
+++ scripts/msig-2-tokenomics.ts
@@ -7,6 +7,9 @@ import * as Time from "../codegen/time.eosn.js"
 import * as Saving from "../codegen/eosio.saving.js"
 import { transaction } from "./msig-helpers.js";
 
+// Flags
+const TESTNET = process.argv[2] === '--testnet';
+
 // newaccounts
 import mware from '../actions/newaccount-eosio.mware.json';
 import wram from '../actions/newaccount-fund.wram.json';
@@ -22,19 +25,21 @@ for ( const setcontract of [ eosio, eosio_token, eosio_fees ] ) {
 }
 
 // 2.1. Unvest B1 tokens (35M EOS NET + 29.6M EOS CPU)
-transaction.actions.push({
-    account: "eosio",
-    name: "unvest",
-    authorization: [{
-        actor: "eosio",
-        permission: "active"
-    }],
-    data: Serializer.encode({object: System.Types.unvest.from({
-        account: "b1",
-        unvest_net_quantity: "35007851.2340 EOS",
-        unvest_cpu_quantity: "29662497.5145 EOS"
-    })}).hexString
-})
+if ( !TESTNET ) {
+    transaction.actions.push({
+        account: "eosio",
+        name: "unvest",
+        authorization: [{
+            actor: "eosio",
+            permission: "active"
+        }],
+        data: Serializer.encode({object: System.Types.unvest.from({
+            account: "b1",
+            unvest_net_quantity: "35007851.2340 EOS",
+            unvest_cpu_quantity: "29662497.5145 EOS"
+        })}).hexString
+    })
+}
 
 // 3.1. Set max supply 2.1B
 transaction.actions.push({
@@ -66,27 +71,29 @@ transaction.actions.push({
 })
 
 // 4.1. Create new bucket accounts
-for ( const newaccount of [ mware, wram ] ) {
-    transaction.actions.push(...newaccount.actions);
-}
+if ( !TESTNET ) {
+    for ( const newaccount of [ mware, wram ] ) {
+        transaction.actions.push(...newaccount.actions);
+    }
 
-// 4.2. Transfer 350M from `eosio` to `fund.wram`
-// 4.3. Transfer 15M from `eosio` to `eosio.mware`
-const transfers = [
-    { from: "eosio", to: "fund.wram", quantity: "350000000.0000 EOS", memo: "EOS Tokenomics" },
-    { from: "eosio", to: "eosio.mware", quantity: "15000000.0000 EOS", memo: "EOS Tokenomics" },
-]
-transfers.forEach(transfer => {
-    transaction.actions.push({
-        account: "eosio.token",
-        name: "transfer",
-        authorization: [{
-            actor: "eosio",
-            permission: "active"
-        }],
-        data: Serializer.encode({object: Token.Types.transfer.from(transfer)}).hexString
-    })
-});
+    // 4.2. Transfer 350M from `eosio` to `fund.wram`
+    // 4.3. Transfer 15M from `eosio` to `eosio.mware`
+    const transfers = [
+        { from: "eosio", to: "fund.wram", quantity: "350000000.0000 EOS", memo: "EOS Tokenomics" },
+        { from: "eosio", to: "eosio.mware", quantity: "15000000.0000 EOS", memo: "EOS Tokenomics" },
+    ]
+    transfers.forEach(transfer => {
+        transaction.actions.push({
+            account: "eosio.token",
+            name: "transfer",
+            authorization: [{
+                actor: "eosio",
+                permission: "active"
+            }],
+            data: Serializer.encode({object: Token.Types.transfer.from(transfer)}).hexString
+        })
+    });
+}
 
 // 5.2. Adjust `inflation_pay_factor=60767` factor ratio
 transaction.actions.push({
@@ -103,7 +110,10 @@ transaction.actions.push({
 })
 
 // 5.3. Set 4 year halvening schedules (up to 20 years, 6 schedules)
-const schedules = [
+const schedules = TESTNET ? [
+    {"start_time": new Date("2024-05-17T00:00:00Z"), "continuous_rate": 0.03617097},
+    {"start_time": new Date("2024-05-21T00:00:00Z"), "continuous_rate": 0.01808549},
+] : [
     {"start_time": new Date("2024-06-01T00:00:00Z"), "continuous_rate": 0.03617097},
     {"start_time": new Date("2028-06-01T00:00:00Z"), "continuous_rate": 0.01808549},
     {"start_time": new Date("2032-06-01T00:00:00Z"), "continuous_rate": 0.00904274},
@@ -135,34 +145,38 @@ transaction.actions.push({
 })
 
 // 5.5. Set MSIG execution time
-transaction.actions.push({
-    account: "time.eosn",
-    name: "checktime",
-    authorization: [{
-        actor: "eosio",
-        permission: "active"
-    }],
-    data: Serializer.encode({object: Time.Types.checktime.from({
-        time: new Date("2024-06-01T00:00:00.000Z")
-    })}).hexString
-})
+if ( !TESTNET ) {
+    transaction.actions.push({
+        account: "time.eosn",
+        name: "checktime",
+        authorization: [{
+            actor: "eosio",
+            permission: "active"
+        }],
+        data: Serializer.encode({object: Time.Types.checktime.from({
+            time: new Date("2024-06-01T00:00:00.000Z")
+        })}).hexString
+    })
+}
 
 // 5.5. Set `eosio.savings` ratio for Staking Rewards/ENF/Labs (53.71% / 29.55% / 16.74%)
-transaction.actions.push({
-    account: "eosio.saving",
-    name: "setdistrib",
-    authorization: [{
-        actor: "eosio.saving",
-        permission: "active"
-    }],
-    data: Serializer.encode({object: Saving.Types.setdistrib.from({
-        accounts: [
-            {account: "eosio.reward", percent: 5371},
-            {account: "eosio.grants", percent: 2955},
-            {account: "eoslabs.io", percent: 1674}
-        ]
-    })}).hexString
-})
+if ( !TESTNET ) {
+    transaction.actions.push({
+        account: "eosio.saving",
+        name: "setdistrib",
+        authorization: [{
+            actor: "eosio.saving",
+            permission: "active"
+        }],
+        data: Serializer.encode({object: Saving.Types.setdistrib.from({
+            accounts: [
+                {account: "eosio.reward", percent: 5371},
+                {account: "eosio.grants", percent: 2955},
+                {account: "eoslabs.io", percent: 1674}
+            ]
+        })}).hexString
+    })
+}
 
 // 6.1 Set incoming fees to 100% go to REX via `donatetorex` strategy
 transaction.actions.push({
@@ -170,7 +184,7 @@ transaction.actions.push({
     name: "setstrategy",
     authorization: [{
         actor: "eosio.fees",
-        permission: "active"
+        permission: "owner"
     }],
     data: Serializer.encode({object: Fees.Types.setstrategy.from({
         "strategy": "donatetorex",
@@ -178,4 +192,4 @@ transaction.actions.push({
     })}).hexString
 })
 
-fs.writeFileSync('actions/msig-2-tokenomics.json', JSON.stringify(transaction, null, 4));
+fs.writeFileSync(`actions/${TESTNET ? "testnet-" : ""}msig-2-tokenomics.json`, JSON.stringify(transaction, null, 4));
