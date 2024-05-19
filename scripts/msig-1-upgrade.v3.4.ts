168906c53c2e8599f69a95b3f897f9b2cbe6241c
Denis Carriere <carriere.denis@gmail.com>
2024-05-19 23:35:55 +0200

    set contracts

--- scripts/msig-1-upgrade.v3.4.ts
+++ scripts/msig-1-upgrade.v3.4.ts
@@ -2,12 +2,13 @@ import * as fs from "fs";
 import { transaction } from "./msig-actions.js";
 
 // setcontracts
-import eosio from '../actions/setcontract-eosio.system.json';
+import eosio_code from '../actions/setcode-eosio.json';
+import eosio_abi from '../actions/setabi-eosio.json';
 import eosio_token from '../actions/setcontract-eosio.token.json';
 import eosio_fees from '../actions/setcontract-eosio.fees.json';
 
 // 1. Deploy new system contracts
-for ( const setcontract of [ eosio, eosio_token, eosio_fees ] ) {
+for ( const setcontract of [ eosio_code, eosio_abi, eosio_token, eosio_fees ] ) {
     transaction.actions.push(...setcontract.actions);
 }
 
